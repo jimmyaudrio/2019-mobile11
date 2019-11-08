f9b9cbb0ff9249532cd955ad18b4dfb4238c5d68
jimmyaudrio <jimmyaudrio16111999@gmail.com>
2019-11-08 14:00:39 +0700

    Menambahkan instansiasi

--- SettingsExample/app/src/main/java/id/ac/polinema/settingsexample/MainActivity.java
+++ SettingsExample/app/src/main/java/id/ac/polinema/settingsexample/MainActivity.java
@@ -16,7 +16,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import id.ac.polinema.settingsexample.fragments.AdvancedFragment;
 import id.ac.polinema.settingsexample.fragments.DialogsFragment;
 import id.ac.polinema.settingsexample.fragments.HomeFragment;
-import id.ac.polinema.settingsexample.fragments.SettingsFragment;
+import id.ac.polinema.settingsexample.fragments.SettingFragment;
 import id.ac.polinema.settingsexample.fragments.WidgetsFragment;
 
 public class MainActivity extends AppCompatActivity
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
 			// TODO: open settings here using openFragment()
+            openFragment(new SettingFragment());
             return true;
         }
 
